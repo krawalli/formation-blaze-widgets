aeff1b88c6fd20a246dacaac88f6db978cdb5c50
quietcreep <dustinchaffin@gmail.com>
2014-10-30 12:55:53 -0700

    Blaze-based widgets for Formation for Meteor

---
+++ package.js
@@ -0,0 +1,63 @@
+Package.describe({
+  name: "quietcreep:formation-blaze-widgets",
+  summary: "Blaze-based HTML widgets for meteor-formation",
+  version: "1.0.0",
+  // git: "http://github.com/quietcreep/meteor-formation"
+});
+
+
+// Npm.depends({
+//   knox: "0.8.5"
+// });
+
+
+Package.onUse( function( api ) {
+
+  api.versionsFrom( 'METEOR@0.9.2.2' );
+
+  var both = [ 'client', 'server' ];
+
+  api.use([ 'quietcreep:formation-core' ], both );
+  api.imply([ 'quietcreep:formation-core' ]);
+
+  api.use([ 'underscore', 'accounts-base', 'tracker' ], both );
+  api.use([ 'templating', 'ui' ], 'client' );
+
+
+  //// fields ///////////////////////
+  var fields = [
+    'boolean',
+    'char',
+    'choice',
+    'date',
+    'datetime',
+    'email',
+    'fileUpload',
+    'modelChoice',
+    'number',
+    'password',
+    'slug',
+    'time',
+    'url'
+  ];
+
+
+  api.addFiles( 'field/widgets.html', 'client' );
+  api.addFiles( 'field/widgets.js', 'client' );
+  api.addFiles( 'field/helpers.js', 'client' );
+  api.addFiles( 'field/extras.html', 'client' );
+  api.addFiles( 'field/extras.js', 'client' );
+
+  for ( var i=0; i < fields.length; i++ ){
+    api.addFiles( ''+ fields[ i ] +'/widget.html', 'client' );
+    api.addFiles( ''+ fields[ i ] +'/widget.js', 'client' );
+  }
+
+});
+
+
+Package.onTest( function( api ) {
+  // api.use( 'tinytest' );
+  // api.use( 'quietcreep:formation' );
+  // api.addFiles( 'quietcreep:formation-tests.js' );
+});
