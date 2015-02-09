f73d9a67c6cd0d2340c6b097469eae1e063233af
quietcreep <dustinchaffin@gmail.com>
2015-02-09 14:45:06 -0700

    Version bumps

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "quietcreep:formation-blaze-widgets",
   summary: "Blaze-based HTML widgets for meteor-formation",
-  version: "2.0.3",
+  version: "2.0.4_1",
   git: "http://github.com/quietcreep/formation-blaze-widgets",
   documentation: null,
 });
@@ -15,8 +15,8 @@ Package.onUse( function( api ) {
 
   api.use('underscore');
 
-  api.use([ 'quietcreep:formation-core@2.0.3' ]);
-  api.imply([ 'quietcreep:formation-core@2.0.3' ]);
+  api.use([ 'quietcreep:formation-core@2.0.4_1' ]);
+  api.imply([ 'quietcreep:formation-core@2.0.4_1' ]);
 
   api.use( 'tsega:bootstrap3-datetimepicker@3.1.3_3', 'client' );
   api.imply( 'tsega:bootstrap3-datetimepicker@3.1.3_3', 'client' );
